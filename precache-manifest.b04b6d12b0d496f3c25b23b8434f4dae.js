f61c590086a28a54da177c9c2ce86b2359ea205d
lakhansamani <lakhan.m.samani@gmail.com>
2018-10-12 17:38:11 +0530

    Updates

---
+++ precache-manifest.b04b6d12b0d496f3c25b23b8434f4dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e0a3fcb036f56bd1acf12a4df84d3c7",
+    "url": "/ak-xyz/static/media/frame.4e0a3fcb.jpg"
+  },
+  {
+    "revision": "2cc19bfd3376687b756b",
+    "url": "/ak-xyz/static/js/runtime~main.2cc19bfd.js"
+  },
+  {
+    "revision": "57c1d98ee4c87038410a",
+    "url": "/ak-xyz/static/js/main.57c1d98e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/ak-xyz/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "57c1d98ee4c87038410a",
+    "url": "/ak-xyz/static/css/main.e7c53d97.chunk.css"
+  },
+  {
+    "revision": "9698a93f64bb0f57d996f5840d4248b4",
+    "url": "/ak-xyz/index.html"
+  }
+];
